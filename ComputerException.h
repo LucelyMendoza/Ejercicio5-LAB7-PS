70a2104ea87d5b44d161dfe72ceca967581d01d1
EybertMacedo <83891586+EybertMacedo@users.noreply.github.com>
2022-12-04 01:26:59 -0500

    Add files via upload

---
+++ ComputerException.h
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include <string>
+
+using namespace std;
+
+string Computer Exception::getMessage(){ /Campo de tipo string message/
+
+return this->message; /puntero this que puede devolver el mensaje/
+
+}
